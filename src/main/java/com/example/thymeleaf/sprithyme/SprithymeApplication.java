cbc6ea9d647a942f24fdcf77bfa1dcc9ed1564b9
Aditya <adityajawanjal27@gmail.com>
2025-04-20 23:44:32 +0530

    Spring Boot + Thymeleaf project setup with H2 DB

---
+++ src/main/java/com/example/thymeleaf/sprithyme/SprithymeApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf.sprithyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprithymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprithymeApplication.class, args);
+	}
+
+}
