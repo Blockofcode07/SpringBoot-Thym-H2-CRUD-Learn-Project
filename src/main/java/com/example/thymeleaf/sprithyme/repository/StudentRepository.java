587f79578f6a3a0d88490023df4e4c028b7cbd8f
Aditya <adityajawanjal27@gmail.com>
2025-04-22 10:24:34 +0530

    Created raw StudentRepository to work with student records in the DB

---
+++ src/main/java/com/example/thymeleaf/sprithyme/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.example.thymeleaf.sprithyme.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.thymeleaf.sprithyme.entity.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, String> {
+
+    
+} 
