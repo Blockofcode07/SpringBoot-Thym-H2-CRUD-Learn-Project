c963edeb6826484601a0d23c1338bf4f94a4ce25
Aditya <adityajawanjal27@gmail.com>
2025-04-23 12:05:14 +0530

    Extended CreateStudentDTO to include id for updating student details in UpdateStudentDTO

---
+++ src/main/java/com/example/thymeleaf/sprithyme/dto/UpdateStudentDETO.java
@@ -0,0 +1,11 @@
+package com.example.thymeleaf.sprithyme.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateStudentDETO extends CreateStudentDTO{
+    
+    private String id;
+}
